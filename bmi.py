55cb4212172f1a7a11aab45c2701637eedb74667
Seoji Lee <cian.lician@gmail.com>
2017-04-29 10:37:43 +0000

    Making get_level function

--- bmi.py
+++ bmi.py
@@ -1,10 +1,4 @@
-while True:
-    height = input('Write your height:')
-    weight = input('Write your weight:')
-    bmi = float(weight) / float(height) ** 2 * 10000
-    BMI_TEXT = '[저체중-18.5-정상-23-과체중-25-비만-30-고도비만]'
-
-
+def get_level(bmi):
     if bmi <= 18.5:
         level = '저체중'
     elif 18.5 <= bmi < 23:
@@ -15,8 +9,14 @@ while True:
         level = '비만'
     else:
         level = '고도비만'
-     
+    return level
+
+while True:
+    height = input('Write your height:')
+    weight = input('Write your weight:')
+    bmi = float(weight) / float(height) ** 2 * 10000
+    BMI_TEXT = '[저체중-18.5-정상-23-과체중-25-비만-30-고도비만]'
      
     # print screen
 
-    print('{}\nYour BMI is {:.2f} \n당신은 {}입니다.'.format(BMI_TEXT, bmi, level))
+    print('{}\nYour BMI is {:.2f} \n당신은 {}입니다.'.format(BMI_TEXT, bmi, get_level(bmi)))
