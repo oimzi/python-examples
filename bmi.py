dae1380c44c89eeaee9adb7ffaa3a9e116728072
Seoji Lee <cian.lician@gmail.com>
2017-04-29 09:06:04 +0000

    Delete bmi_ve2.py bmi_v3.py

--- bmi.py
+++ bmi.py
@@ -1,5 +1,22 @@
 height = input('Write your height:')
 weight = input('Write your weight:')
-bmi = float(weight) / float(height)**2*10000
-print ('Your BMI is {:.2f}'.format(bmi))
+bmi = float(weight) / float(height) ** 2 * 10000
+BMI_TEXT = '[저체중-18.5-정상-23-과체중-25-비만-30-고도비만]'
 
+if bmi <= 18.5:
+     level = '저체중'
+elif 18.5 <= bmi < 23:
+     level = '정상'
+elif 23 <= bmi < 25:
+     level = '과체중'
+elif 25 <= bmi < 30:
+     level = '비만'
+else:
+     level = '고도비만'
+     
+     
+# print screen
+"""
+Tell your BMI 
+"""
+print('{}\nYour BMI is {:.2f} \n당신은 {}입니다.'.format(BMI_TEXT, bmi, level))
